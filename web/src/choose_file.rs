31f4c7aefc9670b948d8a07a19b35d1749de53b3
SOFe <sofe2038@gmail.com>
2021-12-28 21:37:20 +0800

    Added GitHub link ribbon

--- web/src/choose_file.rs
+++ web/src/choose_file.rs
@@ -33,27 +33,36 @@ impl Component for Comp {
 
     fn view(&self, ctx: &Context<Self>) -> Html {
         html! {
-            <section class="section">
-                <div class="container">
-                    <h1 class="title">{ "Phar viewer" }</h1>
-                    <div class="file is-boxed">
-                        <label class="file-label">
-                            <input
-                                class="file-input"
-                                type="file"
-                                ref={&self.input}
-                                onchange={ctx.link().callback(|_| Msg::FileSelected)}
-                                />
-
-                            <span class="file-cta">
-                                <span class="file-label">
-                                    { "Select a phar file" }
-                                </span>
-                            </span>
-                        </label>
-                    </div>
+            <>
+                <div class="x-github-ribbon">
+                    <a href="https://github.com/SOF3/phar.rs">
+                        <img loading="lazy" width="149" height="149"
+                            src="https://github.blog/wp-content/uploads/2008/12/forkme_right_green_007200.png?resize=149%2C149"
+                            class="attachment-full size-full" alt="Fork me on GitHub" data-recalc-dims="1" />
+                    </a>
                 </div>
-            </section>
+                <section class="section">
+                    <div class="container">
+                        <h1 class="title">{ "Phar viewer" }</h1>
+                        <div class="file is-boxed">
+                            <label class="file-label">
+                                <input
+                                    class="file-input"
+                                    type="file"
+                                    ref={&self.input}
+                                    onchange={ctx.link().callback(|_| Msg::FileSelected)}
+                                    />
+
+                                <span class="file-cta">
+                                    <span class="file-label">
+                                        { "Select a phar file" }
+                                    </span>
+                                </span>
+                            </label>
+                        </div>
+                    </div>
+                </section>
+            </>
         }
     }
 }
